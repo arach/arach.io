b0adcdd0ed30851dd2f1ace25d5296ca03c05910
arach <arach@tchoupani.com>
2024-02-25 23:56:04 -0800

    Style Tweaks and beginning to clean up template defaults
    - Add estimated reading time
    - Change default font
    - Change default light theme
    - Add homepage intro message
    - Remove 2 or 3 of the posts, needs further clean upgit config --global user.email git config --global user.namgit config --global user.nam

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://arach.io/", // replace this with your deployed domain
+  author: "Arach Tchoupani",
+  desc: "Arach Tchoupani - Entrepreneur, Engineer, and Investor",
+  title: "Arach.io",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -26,19 +26,19 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/arach",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
     name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://facebook.com/arach",
     linkTitle: `${SITE.title} on Facebook`,
     active: true,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://instagram.com/arach",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
@@ -50,13 +50,13 @@ export const SOCIALS: SocialObjects = [
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:arach@tchoupani.com",
     linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    active: true,
   },
   {
     name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://twitter.com/arach",
     linkTitle: `${SITE.title} on Twitter`,
     active: false,
   },
