77bc0dcf7a69d21255cbb647902bacc7a2dd2e8e
arach <arach@tchoupani.com>
2024-07-09 20:28:42 -0400

    💄 chore: Update Card component styles and add lazy loading for images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,23 +13,32 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
-    className: "text-lg font-bold decoration-dashed hover:underline",
+    className: "text-sm font-bolder decoration-dashed hover:underline",
   };
 
   return (
     <li className="my-6 flex flex-row">
       <div className="flex-1">
-        <a
-          href={href}
-          className="inline-block text-lg font-bold text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-        >
-          {secHeading ? (
-            <h2 {...headerProps}>{title}</h2>
-          ) : (
-            <h3 {...headerProps}>{title}</h3>
-          )}
-        </a>
-        <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
+        <div className="flex flex-row">
+          <div className="flex-2">
+            <a
+              href={href}
+              className="inline-block text-xs font-bold text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0 mr-2 uppercase"
+            >
+
+
+              {secHeading ? (
+                <h2 {...headerProps}>{title}</h2>
+              ) : (
+                <h3 {...headerProps}>{title}</h3>
+              )}
+            </a>
+          </div>
+
+        </div>
+        <div className="flex-1 uppercase">
+          <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
+        </div>
         <div >
 
           <div className="flex-1">
@@ -38,11 +47,13 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
 
         </div>
       </div>
-      {thumbnail && (
-        <div className="item-end px-2 ">
-          <img data-card-thumbnail={true} src={thumbnail.src} alt={title} height={100} width={140} />
-        </div>
-      )}
-    </li>
+      {
+        thumbnail && (
+          <div className="item-end px-2 ">
+            <img data-card-thumbnail={true} src={thumbnail.src} alt={title} height={100} width={140} />
+          </div>
+        )
+      }
+    </li >
   );
 }
